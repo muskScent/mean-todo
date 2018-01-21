81c81e0cb2559130ea26d3ac19c20d2865782948
Othmane Abouelyzza <othmane.abouelyzza@gmail.com>
2018-01-21 10:51:43 +0100

    Fixing database connection settings

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const bcrypt = require('bcryptjs');
 
 const Sequelize = require('sequelize');
 
-var connection = new Sequelize(process.env.DATABASE_URL);
+var connection = new Sequelize(process.env.DATABASE_URL, { dialect:  'mysql' });
 
 // Connecting to database 
 
