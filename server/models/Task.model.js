1512262a32fcfa16c142236f2a5bc6cb0266b0d6
ABOUELYZZA Othmane <o.abouelyzza@Viveris.lan>
2018-01-25 13:05:44 +0100

    Reconfiguring db connection following Sequelize documentation

--- server/models/Task.model.js
+++ server/models/Task.model.js
@@ -1,25 +1,15 @@
-const Sequelize = require('sequelize');
-
-var connection = new Sequelize('tasks_db', 'root', 'root', {
-  host: 'localhost',
-  dialect: 'mysql',
-  define: {
-    timestamps: false
-  }
-});
-
-var Task = connection.define('tasks', {
-  task_id: {
-    type: Sequelize.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-  }, 
-  user_id: {
-    type: Sequelize.INTEGER
-  },
-  task_description: {
-    type: Sequelize.STRING
-  }
-})
-
-module.exports = Task;
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define("tasks", {
+    task_id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    }, 
+    user_id: {
+      type: Sequelize.INTEGER
+    },
+    task_description: {
+      type: Sequelize.STRING
+    }
+  })
+}
